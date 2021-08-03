43209e9294fceff515bb710cacb770ab8f09e82f
casper306 <perfect306@gmail.com>
2021-08-03 22:19:10 +0000

    implement delete

--- express_server.js
+++ express_server.js
@@ -14,8 +14,8 @@ app.set('view engine', 'ejs');
 function generateRandomString() {
   let character = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let result = '';
-  for (let i = 0; i < 6 ; i++) 
-  result += character[Math.floor(Math.random() * character.length)];
+  for (let i = 0; i < 6; i++)
+    result += character[Math.floor(Math.random() * character.length)];
   return result;
 }
 
@@ -43,7 +43,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-   //console.log(req.body);  // Log the POST request body to the console
+  //console.log(req.body);  // Log the POST request body to the console
   // res.send("Ok");
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
@@ -63,6 +63,15 @@ app.get("/u/:shortURL", (req, res) => {
 app.get('/hello', (reg, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  console.log('deleting', shortURL);
+  delete urlDatabase[shortURL];
+  console.log(urlDatabase);
+  res.redirect('/urls');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
